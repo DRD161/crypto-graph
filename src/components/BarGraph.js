192eb28c349464e7d57d6aee49911b4bdf88845e
Dylan <dylan@dylandavenport.com>
2019-09-20 07:02:18 -0400

    Added bar and doughnut graph

---
+++ src/components/BarGraph.js
@@ -0,0 +1,116 @@
+import React, { Component } from "react";
+import Chart from "chart.js";
+
+class BarGraph extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { chart: null };
+  }
+  chart = React.createRef();
+
+  componentDidMount() {
+    const myChartRef = this.chart.current.getContext("2d");
+    const gradient = myChartRef.createLinearGradient(20, 500, 10, 20);
+    gradient.addColorStop(0, "#75C6FF");
+    gradient.addColorStop(1, "#3E0B80");
+    // Custom plugin to change chart area background color
+    Chart.pluginService.register({
+      beforeDraw: function(chart) {
+        if (
+          chart.config.options.chartArea &&
+          chart.config.options.chartArea.backgroundColor
+        ) {
+          const ctx = chart.chart.ctx;
+          const chartArea = chart.chartArea;
+
+          ctx.save();
+          ctx.fillStyle = chart.config.options.chartArea.backgroundColor;
+          ctx.fillRect(
+            chartArea.left,
+            chartArea.top,
+            chartArea.right - chartArea.left,
+            chartArea.bottom - chartArea.top
+          );
+          ctx.restore();
+        }
+      }
+    });
+    let theChart = new Chart(myChartRef, {
+      type: "bar",
+      data: {
+        labels: this.props.chartLabels,
+        datasets: [
+          {
+            backgroundColor: gradient,
+            pointBackgroundColor: "#fff",
+            pointBorderColor: gradient,
+            pointRadius: "5",
+            hoverBackgroundColor: "#75C6FF",
+            hoverBorderColor: gradient,
+            data: this.props.chartData
+          }
+        ]
+      },
+      options: {
+        tooltips: {
+          // callback function to add $ sign to tooltips
+          callbacks: {
+            label: function(tooltipItems) {
+              return "$" + tooltipItems.yLabel.toLocaleString();
+            }
+          }
+        },
+        responsive: true,
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [
+            {
+              gridLines: {
+                display: false
+              },
+              ticks: {
+                padding: 10,
+                fontColor: "#87889C"
+              }
+            }
+          ],
+          yAxes: [
+            {
+              gridLines: {
+                color: "#535356"
+              },
+              ticks: {
+                // callback function to add $ to y-axis
+                userCallback: function(tick) {
+                  return " $" + tick.toLocaleString();
+                },
+                fontColor: "#87889C"
+              }
+            }
+          ]
+        }
+      }
+    });
+    this.setState({ chart: theChart });
+  }
+  componentWillReceiveProps(nextProps) {
+    // update chart according to prop change
+    this.state.chart.data.datasets.forEach(dataset => {
+      dataset.data.push(nextProps.chartData);
+      dataset.data.push(nextProps.chartLabels);
+      this.state.chart.update();
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <canvas id="myBarChart" ref={this.chart} />
+      </div>
+    );
+  }
+}
+
+export default BarGraph;
